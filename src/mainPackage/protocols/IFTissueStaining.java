86191eb961c7af9e54765dca268b729b867bd2fa
Victoria <Victoria@MSI>
2022-03-24 18:39:43 +0200

    Present as a general method

--- src/mainPackage/protocols/IFTissueStaining.java
+++ src/mainPackage/protocols/IFTissueStaining.java
@@ -12,31 +12,31 @@ import mainPackage.utils.RGB;
  *
  * @author aritchie
  */
-public class Cytokeratin extends Protocol {
+public class IFTissueStaining extends Protocol {
 
     @Override
     public String getName() {
-        return "Measure cytokeratin-stained tissue areas";
+        return "Measure immunofluorescence-stained tissue areas";
     }
 
     @Override
     protected ControlReference[] getParameters() {
         return new ControlReference[]{
             new ControlReference(LAYER, "Raw image is on which layer"),
-            new ControlReference(COMBO, new String[]{"RED", "GREEN", "BLUE"}, "The CK8 is stained with which colour", 1),
-            new ControlReference(COMBO, new String[]{"RED", "GREEN", "BLUE"}, "The CK14 is stained with which colour", 0),
-            new ControlReference(SLIDER, "The CK14 should be how strong to be positive", 25),
-            new ControlReference(SLIDER, "The CK8 should be how strong for double positivity", 40),
+            new ControlReference(COMBO, new String[]{"RED", "GREEN", "BLUE"}, "The first stain is stained with which colour", 1),
+            new ControlReference(COMBO, new String[]{"RED", "GREEN", "BLUE"}, "The second stain is stained with which colour", 0),
+            new ControlReference(SLIDER, "The second stain  should be how strong to be positive", 25),
+            new ControlReference(SLIDER, "The first stain  should be how strong for double positivity", 40),
             new ControlReference(SLIDER, new Integer[]{0, 20}, "How sensitive the tissue separator should be", 8)};
     }
 
     @Override
     protected Processor getProcessor() {
-        double ck8ValThrshld = (100 - param.slider[0]) * 0.01; //threshold range 0-100;
-        double ck14Thrshld = param.slider[1] * (255. / 100); //threshold range 0-255;
+        double fthresh = (100 - param.slider[0]) * 0.01; //threshold range 0-100;
+        double sthresh = param.slider[1] * (255. / 100); //threshold range 0-255;
         int tissueThrshld = param.slider[2]; //threshold range 0-20;
 
-        return new ProcessorFast("CK8/14", 7) {
+        return new ProcessorFast("IF Stains", 7) {
 
             ImageData layer;
             int[] holeGrid;
@@ -52,18 +52,18 @@ public class Cytokeratin extends Protocol {
             @Override
             protected void methodCore(int p) {
                 int col = inImage[0].pixels32[p];
-                int ck8 = COL.comboColorSelector(param.combo[0], col);
-                int ck14 = COL.comboColorSelector(param.combo[1], col);
-                double ck8ck14ratio = 1.25; //what is considered to be positive
+                int fstain = COL.comboColorSelector(param.combo[0], col);
+                int sstain = COL.comboColorSelector(param.combo[1], col);
+                double stainratio = 1.25; //what is considered to be positive
                 int bright = RGB.brightness(col);
                 outImage[0].pixels32[p] = layer.pixels32[p] == COL.BLACK ? COL.BLACK
-                        : (ck14 * ck8ck14ratio >= ck8) && (bright > ck14Thrshld)
-                                ? ck8 > ck14 * ck8ValThrshld ? COL.YELLOW : COL.RED : COL.GREEN;
+                        : (sstain * stainratio >= fstain) && (bright > sthresh)
+                                ? fstain > sstain * fthresh ? COL.YELLOW : COL.RED : COL.GREEN;
             }
 
             @Override
             protected void methodFinal() {
-                datas.add(Counters.countRGBCMYK().runSingle(sourceImage.imageName, outImage[0]));
+                addResultData(Counters.countRGBCMYK().runSingle(sourceImage, outImage[0]));
             }
 
         };
